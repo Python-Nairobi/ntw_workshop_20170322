af8e47aeee844e7fb73ba196aaaa9e9d216d8ac5
Matt Gathu <mattgathu@gmail.com>
2017-03-21 21:06:05 +0300

    [dedug] fixed os env issue
    * added env variables container
    * code formatting

--- tw_stream_consumer/stream_consumer.py
+++ tw_stream_consumer/stream_consumer.py
@@ -1,13 +1,32 @@
 import time
 from umqtt.simple import MQTTClient
-from os import environ as env
+
+# ============================================================================
+# environment container
+# ============================================================================
+env = {
+    "MQTT_USER": "",
+    "MQTT_PASSWORD": "",
+    "MQTT_PORT": "",
+    "TW_STREAM_TOPIC": "",
+}
+
 
 # Received messages from subscriptions will be delivered to this callback
 def sub_cb(topic, msg):
     print((topic, msg))
 
-def main(server=env['MQTT_SERVER'], user=env['MQTT_USER'], password=env['MQTT_PASSWORD'],port=env['MQTT_PORT']):
-    c = MQTTClient("umqtt_client",server, user=user,password=password, port=port)
+
+def main(server=env['MQTT_SERVER'],
+         user=env['MQTT_USER'],
+         password=env['MQTT_PASSWORD'],
+         port=env['MQTT_PORT']):
+
+    c = MQTTClient("umqtt_client",
+                   server,
+                   user=user,
+                   password=password,
+                   port=port)
     c.set_callback(sub_cb)
     c.connect()
     c.subscribe(bytes(env['TW_STREAM_TOPIC']))
@@ -24,7 +43,6 @@ def main(server=env['MQTT_SERVER'], user=env['MQTT_USER'], password=env['MQTT_PA
 
     c.disconnect()
 
+
 if __name__ == "__main__":
     main()
-
-
